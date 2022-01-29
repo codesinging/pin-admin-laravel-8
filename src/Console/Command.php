5765f3253774491f1680b50545640f2934370990
codesinging <codesinging@gmail.com>
2022-01-29 23:08:04 +0800

    新增基础命令类

---
+++ src/Console/Command.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Console;
+
+class Command extends \Illuminate\Console\Command
+{
+    use FileHelpers;
+
+    /**
+     * 输出一行标题信息
+     *
+     * @param string $title
+     * @param string $prefix
+     *
+     * @return void
+     */
+    protected function title(string $title, string $prefix = '- ')
+    {
+        $this->line($prefix . $title);
+    }
+}
