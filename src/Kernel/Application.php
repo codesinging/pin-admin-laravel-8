d76a9c6d04f575cf5200580397bb26e41d64c5dd
codesinging <codesinging@gmail.com>
2022-01-29 22:46:58 +0800

    添加 PinAdmin 应用类，增加 PinAdmin 关于应用的方法

---
+++ src/Kernel/Application.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Kernel;
+
+use Illuminate\Config\Repository;
+use Illuminate\Support\Str;
+
+class Application
+{
+    /**
+     * PinAdmin 应用的基础目录
+     */
+    const BASE_DIRECTORY = PinAdmin::BASE_DIRECTORY;
+
+    /**
+     * 应用路由文件
+     *
+     * @var string
+     */
+    protected static string $routeFilename = 'routes.php';
+
+    /**
+     * 应用名称
+     *
+     * @var string
+     */
+    protected string $name;
+
+    /**
+     * @var string 应用目录
+     */
+    protected string $directory;
+
+    /**
+     * 应用启动参数
+     *
+     * @var array
+     */
+    protected array $options;
+
+    /**
+     * @param string $name
+     * @param array $options
+     */
+    public function __construct(string $name, array $options = [])
+    {
+        $this->name = $name;
+        $this->options = $options;
+        $this->directory = self::BASE_DIRECTORY . DIRECTORY_SEPARATOR . Str::studly($name);
+    }
+
+    /**
+     * 返回应用名
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * 获取应用目录，相对于 `app` 目录
+     *
+     * @param ...$paths
+     *
+     * @return string
+     */
+    public function directory(...$paths): string
+    {
+        array_unshift($paths, $this->directory);
+        return implode(DIRECTORY_SEPARATOR, $paths);
+    }
+
+    /**
+     * 获取应用的路径
+     *
+     * @param ...$paths
+     *
+     * @return string
+     */
+    public function path(...$paths): string
+    {
+        return app_path($this->directory(...$paths));
+    }
+
+    /**
+     * 获取应用的命名空间
+     *
+     * @param ...$paths
+     *
+     * @return string
+     */
+    public function getNamespace(...$paths): string
+    {
+        return implode('\\', ['App', str_replace('/', '\\', $this->directory(...$paths))]);
+    }
+
+    /**
+     * 获取 PinAdmin 配置值
+     *
+     * @param string|null $key
+     * @param null|mixed $default
+     *
+     * @return $this|array|mixed
+     */
+    public function config(string $key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return config($this->name());
+        }
+
+        return config($this->name() . '.' . $key, $default);
+    }
+
+    /**
+     * 获取应用的路由前缀
+     *
+     * @return string
+     */
+    public function routePrefix(): string
+    {
+        return $this->options['prefix'] ?? $this->name();
+    }
+
+    /**
+     * 获取应用的绝对链接地址
+     *
+     * @param string $path
+     * @param array $parameters
+     *
+     * @return string
+     */
+    public function link(string $path = '', array $parameters = []): string
+    {
+        $link = '/' . $this->routePrefix();
+        $path and $link .= Str::start($path, '/');
+        $parameters and $link .= '?' . http_build_query($parameters);
+
+        return $link;
+    }
+}
--- src/Kernel/PinAdmin.php
+++ src/Kernel/PinAdmin.php
@@ -29,6 +29,32 @@ class PinAdmin
     const SLOGAN = 'A Laravel package to rapidly build administrative applications';
 
     /**
+     * PinAdmin 应用基础目录，相对于 `app`
+     */
+    const BASE_DIRECTORY = 'PinAdmin';
+
+    /**
+     * 应用索引文件名
+     *
+     * @var string
+     */
+    protected static string $indexFilename = 'indexes.php';
+
+    /**
+     * 所有的 PinAdmin 应用实例
+     *
+     * @var Application[]
+     */
+    protected array $applications = [];
+
+    /**
+     * 当前路由所在的 PinAdmin 应用
+     *
+     * @var Application
+     */
+    protected Application $application;
+
+    /**
      * 获取 PinAdmin 标记
      *
      * @param string|null $suffix
@@ -39,5 +65,128 @@ class PinAdmin
     public function label(string $suffix = null, string $separator = '_'): string
     {
         return self::LABEL . ($suffix ? $separator . $suffix : '');
+    }
+
+    /**
+     * 获取 PinAdmin 包路径
+     *
+     * @param ...$paths
+     *
+     * @return string
+     */
+    public function packagePath(...$paths): string
+    {
+        array_unshift($paths, dirname(__DIR__, 2));
+        return implode(DIRECTORY_SEPARATOR, $paths);
+    }
+
+    /**
+     * 返回 PinAdmin 基础目录，相对于 `app`
+     *
+     * @param ...$paths
+     *
+     * @return string
+     */
+    public function baseDirectory(...$paths): string
+    {
+        array_unshift($paths, self::BASE_DIRECTORY);
+        return implode(DIRECTORY_SEPARATOR, $paths);
+    }
+
+    /**
+     * 返回 PinAdmin 的基础路径
+     *
+     * @param ...$paths
+     *
+     * @return string
+     */
+    public function basePath(...$paths): string
+    {
+        return app_path($this->baseDirectory(...$paths));
+    }
+
+    /**
+     * 是否已经安装 PinAdmin 包
+     *
+     * @return bool
+     */
+    public function isInstalled(): bool
+    {
+        return file_exists($this->basePath(self::$indexFilename));
+    }
+
+    /**
+     * 返回应用索引数据
+     *
+     * @return array
+     */
+    public function indexes(): array
+    {
+        if ($this->isInstalled()) {
+            return include($this->basePath(self::$indexFilename));
+        }
+        return [];
+    }
+
+    /**
+     * 启动指定名称的 PinAdmin 应用
+     *
+     * @param string $name
+     * @param array $configs
+     *
+     * @return $this
+     */
+    public function boot(string $name, array $configs = []): self
+    {
+        empty($this->applications[$name]) and $this->applications[$name] = new Application($name, $configs);
+        $this->application = $this->applications[$name];
+        return $this;
+    }
+
+    /**
+     * 返回当前 PinAdmin 应用
+     *
+     * @return Application
+     */
+    public function application(): Application
+    {
+        return $this->application;
+    }
+
+    /**
+     * 返回所有 PinAdmin 应用
+     *
+     * @return Application[]
+     */
+    public function applications(): array
+    {
+        return $this->applications;
+    }
+
+    /**
+     * 初始化所有应用
+     *
+     * @return void
+     */
+    protected function initialize()
+    {
+        foreach ($this->indexes() as $name => $options) {
+            if ($options['status']) {
+                $this->boot($name, $options);
+            }
+        }
+    }
+
+    /**
+     * 调用 PinAdmin 应用的方法
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return $this->application->$name(...$arguments);
     }
 }
