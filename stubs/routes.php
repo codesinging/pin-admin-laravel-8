4a03a892d08f27ad6f4cf7bf8c94903bb7c3841f
codesinging <codesinging@gmail.com>
2022-01-30 23:20:55 +0800

    修改 routes 模板

--- stubs/routes.php
+++ stubs/routes.php
@@ -4,5 +4,17 @@
  * Github: https://github.com/codesinging
  */
 
+use CodeSinging\PinAdmin\Kernel\Admin;
 use Illuminate\Support\Facades\Route;
 
+Admin::boot('__DUMMY_NAME__')->guestRoutes(function () {
+
+    Route::get('/', function () {
+        dump(Admin::config()->all());
+    });
+
+});
+
+Admin::boot('__DUMMY_NAME__')->authRoutes(function () {
+
+});
