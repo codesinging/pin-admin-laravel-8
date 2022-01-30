587a67990c0d0a8017b994bdacb95dac9e5199fa
codesinging <codesinging@gmail.com>
2022-01-31 00:20:20 +0800

    创建基础模型类 BaseModel 和 AuthModel

---
+++ src/Model/AuthModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Model;
+
+use Illuminate\Foundation\Auth\User;
+
+class AuthModel extends User
+{
+
+}
---
+++ src/Model/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+
+}
