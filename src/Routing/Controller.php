578c7a086983ffa01ef79adb2e1312ccc3e9d3de
codesinging <codesinging@gmail.com>
2022-01-31 13:50:27 +0800

    基础控制器增加 view 方法

--- src/Routing/Controller.php
+++ src/Routing/Controller.php
@@ -6,6 +6,10 @@
 
 namespace CodeSinging\PinAdmin\Routing;
 
+use CodeSinging\PinAdmin\Kernel\Admin;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -13,4 +17,21 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 class Controller extends \Illuminate\Routing\Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * 返回视图内容
+     *
+     * @param string|null $view
+     * @param array $data
+     * @param array $mergeData
+     *
+     * @return Application|Factory|View
+     */
+    protected function view(string $view = null, array $data = [], array $mergeData = [])
+    {
+        if ($view) {
+            $view = Admin::name() . '.' . $view;
+        }
+        return view($view, $data, $mergeData);
+    }
 }
