b0e0f006c35f02541a4fdabdd8a0275cd71f614b
codesinging <codesinging@gmail.com>
2022-02-12 01:25:02 +0800

    重构 http.js

--- resources/js/utils/http.js
+++ resources/js/utils/http.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from 'axios'
 import config from '../config/http'
 import state from "../store/state"
 import message from "./message"
@@ -7,70 +7,118 @@ const defaultLabel = 'loading'
 
 const http = axios.create(config)
 
-http.interceptors.request.use(
-    config => {
-        state().set(defaultLabel)
+// 设置请求状态
+const setStates = config => {
+    state().set(defaultLabel)
 
-        if (config?.label) {
-            state().set(config.label)
-        }
-
-        return config
-    },
-    error => {
-        console.log('http.request.error', error)
-        message.error('发送网络请求错误')
+    if (config?.label) {
+        state().set(config.label)
     }
-)
+}
 
-http.interceptors.response.use(
-    response => {
-        state().unset(defaultLabel)
+// 恢复请求状态
+const unsetState = config => {
+    state().unset(defaultLabel)
 
-        if (response?.config?.label) {
-            state().unset(response.config.label)
-        }
+    if (config?.label) {
+        state().unset(config.label)
+    }
+}
 
-        if (response.status === 200) {
-            if (response.data.code === 0) {
-                if (response?.config?.message !== false && response?.data?.message) {
-                    message.success(response?.config?.message || response.data.message)
-                }
-                if (response.data.data !== undefined) {
-                    return response.data.data
+// 控制台打印日志
+const errorLog = (title, content) => {
+    console.log(title, content)
+}
+
+// 提示成功信息
+const showSuccess = (content, config) => {
+    if (config.message || config.successMessage) {
+        message.success(typeof config.successMessage === 'string' ? config.successMessage : content)
+    }
+}
+
+// 提示失败信息
+const showError = (content, config) => {
+    if (config.message || config.errorMessage) {
+        message.error(typeof config.errorMessage === 'string' ? config.errorMessage : content)
+        errorLog('http config', config)
+    }
+}
+
+// 初始化请求配置
+const initConfig = config => {
+    config = typeof config === 'string' ? {label: config} : config
+
+    config.message = config.message ?? true
+    config.successMessage = config.successMessage ?? true
+    config.errorMessage = config.errorMessage ?? true
+    config.catch = config.catch ?? false
+
+    return config
+}
+
+// 请求处理
+const handler = (request, config) => {
+    return new Promise(((resolve, reject) => {
+        request.then(res => {
+            if (res.status === 200) {
+                if (res?.data?.code === 0) {
+                    showSuccess(res?.data?.message, config)
+                    resolve(res?.data?.data)
+                    return
                 }
             }
-        }
 
-        message.error(`[${response.data.code}]${response.data.message || response.statusText || '请求响应错误'}`)
-        console.log('http.response.error', response)
+            let error = `[${res?.data?.code}]${res?.data.message || res.statusText || '请求响应错误'}`
 
-        if (response?.config?.catch) {
-            return Promise.reject(response)
-        }
-    },
-    error => {
-        error = error.toJSON()
+            showError(error, config)
+            errorLog('http response status error', res)
 
-        state().unset(defaultLabel)
-
-        if (error.config?.label) {
-            state().unset(error.config.label)
-        }
-
-        console.log('http.response.status.error', error)
-
-        if (error.config.message !== false) {
-            if (error.status === null) {
-                message.error('网络或服务器连接错误')
-            } else {
-                message.error(`[${error.status}]请求响应状态异常`)
+            if (config.catch) {
+                reject(error)
             }
-        }
-        if (error.config.catch) {
-            return Promise.reject(error)
-        }
-    }
-)
+        }).catch(error => {
+            let content = error.status === null ? '网络或服务器连接错误' : `[${error?.status}]${error?.response?.data?.message || error?.response?.message || '请求响应错误'}`
 
-export default http
+            showError(content, config)
+            errorLog('http response error', error.toJSON())
+
+            if (config.catch) {
+                reject(content)
+            }
+        }).finally(() => {
+            unsetState(config)
+        })
+    }))
+}
+
+const get = (url, config = {}) => {
+    config = initConfig(config)
+    setStates(config)
+    return handler(http.get(url, config), config)
+}
+
+const del = (url, config = {}) => {
+    config = initConfig(config)
+    setStates(config)
+    return handler(http.delete(url, config), config)
+}
+
+const post = (url, data, config = {}) => {
+    config = initConfig(config)
+    setStates(config)
+    return handler(http.post(url, data, config), config)
+}
+
+const put = (url, data, config = {}) => {
+    config = initConfig(config)
+    setStates(config)
+    return handler(http.put(url, data, config), config)
+}
+
+export default {
+    get,
+    del,
+    post,
+    put,
+}
