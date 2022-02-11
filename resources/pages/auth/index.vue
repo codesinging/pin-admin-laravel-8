a2ec388fafc97693b8d6cd8d43d910d10170dba9
codesinging <codesinging@gmail.com>
2022-02-11 13:01:40 +0800

    完善登录功能

--- resources/pages/auth/index.vue
+++ resources/pages/auth/index.vue
@@ -2,10 +2,10 @@
     <div class="h-screen flex items-center justify-center">
         <el-card header="管理员登录" class="w-96">
             <el-form :model="data" :rules="rules" ref="form">
-                <el-form-item prop="mobile">
-                    <el-input v-model="data.mobile">
+                <el-form-item prop="name">
+                    <el-input v-model="data.name">
                         <template #prepend>
-                            <div class="w-14 text-center">手机号码</div>
+                            <div class="w-14 text-center">用户名称</div>
                         </template>
                     </el-input>
                 </el-form-item>
@@ -13,7 +13,7 @@
                 <el-form-item prop="password">
                     <el-input v-model="data.password" show-password>
                         <template #prepend>
-                            <div class="w-14 text-center">密码</div>
+                            <div class="w-14 text-center">登录密码</div>
                         </template>
                     </el-input>
                 </el-form-item>
@@ -31,17 +31,18 @@ import {reactive, ref} from "vue";
 import useState from "../../js/store/state";
 import message from "../../js/utils/message";
 import apis from "../../apis";
+import {homeUrl} from '../../env'
 
 const state = useState()
 
 const data = reactive({
-    mobile: '',
+    name: '',
     password: '',
 })
 
 const rules = {
-    mobile: [
-        {required: true, message: '手机号码不能为空'}
+    name: [
+        {required: true, message: '用户名称不能为空'}
     ],
     password: [
         {required: true, message: '登录密码不能为空'}
@@ -52,9 +53,9 @@ const form = ref()
 
 const onSubmit = () => {
     form.value.validate(valid => {
-        if (valid){
-            apis.auth.login(data).then(res=>{
-                console.log(res)
+        if (valid) {
+            apis.auth.login(data).then(res => {
+                location.href = homeUrl
             })
         } else {
             message.warning('表单验证失败')
--- stubs/controllers/AuthController.php
+++ stubs/controllers/AuthController.php
@@ -6,12 +6,33 @@
 
 namespace __DUMMY_NAMESPACE__\Controllers;
 
+use CodeSinging\PinAdmin\Kernel\Admin;
 use CodeSinging\PinAdmin\Routing\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function index()
     {
         return page('auth/index');
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if (Admin::auth()->attempt($credentials)) {
+            return $this->success('登录成功');
+        }
+
+        if (!Admin::user()['status']) {
+            return $this->error('用户状态异常');
+        }
+
+        return $this->error('账号和密码不匹配');
     }
 }
