c39b307c29c38f9e3bc474f0c8888ae85a585099
codesinging <codesinging@gmail.com>
2022-02-11 13:10:04 +0800

    完善登录功能，增加登录请求路由

--- stubs/routes/web.php
+++ stubs/routes/web.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 Admin::boot('__DUMMY_NAME__')
     ->routeGroup(function () {
 
-        Route::get('/auth', [__DUMMY_NAMESPACE__\Controllers\AuthController::class, 'index']);
+        Route::get('auth', [__DUMMY_NAMESPACE__\Controllers\AuthController::class, 'index']);
+        Route::post('auth/login', [__DUMMY_NAMESPACE__\Controllers\AuthController::class, 'login']);
 
     }, false)
     ->routeGroup(function () {
