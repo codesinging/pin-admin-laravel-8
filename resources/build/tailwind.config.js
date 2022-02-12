a65d81fd5b588257c3716ff335e8e1b4dca1be56
codesinging <codesinging@gmail.com>
2022-02-12 13:28:54 +0800

    tailwind.config.js 中的 content 增加 components 目录

--- resources/build/tailwind.config.js
+++ resources/build/tailwind.config.js
@@ -3,6 +3,7 @@ const { srcPath } = require('../env')
 module.exports = {
   content: [
       srcPath + '/pages/**/*.vue',
+      srcPath + '/components/**/*.vue',
       srcPath + '/views/**/*.blade.php',
   ],
   theme: {
